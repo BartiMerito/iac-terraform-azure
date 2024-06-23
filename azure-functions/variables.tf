09e8d44ab5a336ee1fc798aee3ad1691d6aab2f6
BartiMerito <152792538+BartiMerito@users.noreply.github.com>
2024-06-23 13:09:12 +0200

    Create variables.tf

---
+++ azure-functions/variables.tf
@@ -0,0 +1,61 @@
+
+variable "druga_komenda" {
+  type    = string
+  default = "location"
+}
+
+variable "pierwsza_funkcja" {
+  type    = string
+  default = "location"
+}
+
+variable "funkcja_druga" {
+  type    = string
+  default = "location"
+}
+
+variable "funkcja_resource" {
+  type    = string
+  default = "lokacja3"
+}
+
+variable "linux_funkcja_test" {
+  type    = string
+  default = "location"
+}
+
+variable "przyklad_service_plan" {
+  type    = string
+  default = "location"
+}
+
+variable "example-linux-function-app" {
+  type    = string
+  default = "location"
+}
+
+variable "azure-functions-test-rg" {
+  type    = string
+  default = "location"
+}
+
+variable "functionsapptestsa" {
+  type    = string
+  default = "location"
+}
+
+variable "azure-functions-test-service-plan" {
+  type    = string
+  default = "location"
+}
+
+variable "test-azure-functions" {
+  type    = string
+  default = "location"
+}
+
+
+variable "id_for_app" {
+  type    = string
+  default = "przykladfunkcji"
+}
